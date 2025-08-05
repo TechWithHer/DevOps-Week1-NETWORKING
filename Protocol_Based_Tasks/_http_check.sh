bdb1e373268bc43a5386d598419cbc0c3f533c54
Ayushi <ayushi2530@gmail.com>
2025-08-05 14:00:15 +0800

    fdhf

---
+++ Protocol_Based_Tasks/_http_check.sh
@@ -0,0 +1,5 @@
+echo "Checking the HTTP Request of EXAMPLE.COM - TEST WEB SERVER AVAILABILITY "
+
+curl -I http://example.com
+
+
---
+++ Protocol_Based_Tasks/ping.sh
@@ -0,0 +1,4 @@
+echo "Welcome to ICMP (PING) - Checking Host Reachability"
+echo "Pinging GOOGLE.COM for 4 sessions only"
+ping -c 4 google.com
+
